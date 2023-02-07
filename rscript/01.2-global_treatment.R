9f7448d91f38262bf995c2917a4fbb560c54648f
Lincoln <mr.berlin.lin@gmail.com>
2023-02-07 17:14:32 +0800

    0207_17pm_GIRC

--- rscript/01.2-global_treatment.R
+++ rscript/01.2-global_treatment.R
@@ -658,7 +658,7 @@ table_02 <-
   gsub("font-size: initial !important;", 
        "font-size: 15pt !important;", 
        .) %>% 
-  scroll_box(height = "500px", width = "100%")
+  scroll_box(height = "500px", width = "800px")
 
 
 

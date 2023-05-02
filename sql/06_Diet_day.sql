9f30e519aa2298c4b835cd4bb912b69a3a452b33
JFree <jeffrey410469@gmail.com>
2023-05-02 16:41:16 +0800

    - Update 06 diet day

--- sql/06_Diet_day.sql
+++ sql/06_Diet_day.sql
@@ -25,7 +25,7 @@ WITH client_diet_by_date AS (
   GROUP BY notes.client_id, notes.date, clients.mobile
 )
 SELECT DISTINCT ON (client_diet_by_date.client_id, client_diet_by_date.date_diet)
-  client_diet_by_date.*, client_targets.begin_date, client_targets.end_date, client_targets.calorie AS calorie_target, client_targets.updated_at AS target_updated_at
+  client_diet_by_date.*, client_targets.begin_date, client_targets.end_date, client_targets.protein AS protein_target, client_targets.fat AS fat_target, client_targets.carbohydrate AS carbonhyrdate_target, client_targets.calorie AS calorie_target, client_targets.updated_at AS target_updated_at
 FROM client_diet_by_date
 LEFT JOIN LATERAL (
   SELECT * FROM consulting_client_targets
@@ -33,3 +33,6 @@ LEFT JOIN LATERAL (
 ) AS client_targets
   ON client_targets.client_id = client_diet_by_date.client_id
  AND client_diet_by_date.date_diet BETWEEN client_targets.begin_date AND client_targets.end_date
+
+
+加上目標 protein, fat, carbohydrate, calorie
